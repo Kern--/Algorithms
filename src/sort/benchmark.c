0dd174c99beb63c68f41b0017f03f7f3fbd87f11
Kern <kern.walster@gmail.com>
2017-05-03 21:38:13 -0500

    Switch benchmark to use SortFuncInfo

--- src/sort/benchmark.c
+++ src/sort/benchmark.c
@@ -2,40 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 #include "sort.h"
+#include "test.h"
 #include "util.h"
 
+#define len(a) sizeof (a) / sizeof (a[0])
+
 // A method to test stuff
-void test(SortFunc sort, const size_t arraySize, const size_t runs);
+void test(SortFuncInfo sort, const size_t arraySize, const size_t runs);
+
+static SortFuncInfo sorts[] = {
+    { bubbleSort, "Bubble Sort" },
+    { insertionSort, "Insertion Sort" },
+    { selectionSort, "Selection Sort" },
+    { mergeSort, "Merge Sort" },
+    { quickSort, "Quick Sort" }
+};
+
 
 int main() {
     const size_t array_size = 30000;
     const size_t runs = 10;
 
-    printf("Bubble Sort\n");
-    test(&bubbleSort, array_size, runs);
-
-    printf("\n\nInsertion Sort\n");
-    test(&insertionSort, array_size, runs);
-
-    printf("\n\nSelection Sort\n");
-    test(&insertionSort, array_size, runs);
-
-    printf("\n\nMerge Sort\n");
-    test(&mergeSort, array_size, runs);
-
-    printf("\n\nQuick Sort\n");
-    test(&quickSort, array_size, runs);
+    size_t numSorts = len(sorts);
+    for (size_t i = 0; i < numSorts; ++i) {
+        test(sorts[i], array_size, runs);   
+    }
 }
 
-void test(SortFunc sort, const size_t arraySize, const size_t runs) {
+void test(SortFuncInfo sort, const size_t arraySize, const size_t runs) {
     volatile clock_t start;
     volatile clock_t end;
     clock_t *timing = (clock_t *)malloc(runs * sizeof (clock_t));
     clock_t average = 0;
+    
+    printf("%s\n", sort.name); 
     for (size_t i = 0; i < runs; ++i) {
         int *array = generateRandomArray(arraySize);
         start = clock();
-        sort(array, arraySize);
+        sort.sort(array, arraySize);
         end = clock();
         timing[i] = end - start;
         average += timing[i];
@@ -43,7 +47,7 @@ void test(SortFunc sort, const size_t arraySize, const size_t runs) {
         free(array);
     }
     average /= runs;
-    printf("\nAVERAGE: %lu cycles (%lu ms)\n", average, average * 1000 / CLOCKS_PER_SEC);
+    printf("\nAVERAGE: %lu cycles (%lu ms)\n\n", average, average * 1000 / CLOCKS_PER_SEC);
     free(timing);
 }
 
